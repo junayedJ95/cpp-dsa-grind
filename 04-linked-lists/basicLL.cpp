ce809bf926003d0cf9cf9d3aaa09e1966bbc564b
JuNaYeD <junayedpsrl@gmail.com>
2026-08-06 22:05:32 +0600

    Implement basic linked list structure and traversal

---
+++ 04-linked-lists/basicLL.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+// ------------------------------
+// Node structure
+// Each node has:
+// 1. data
+// 2. pointer to next node
+// ------------------------------
+struct Node {
+    int data;
+    Node* next;
+};
+
+int main() {
+
+    // ------------------------------
+    // Create three nodes dynamically
+    // ------------------------------
+    Node* first = new Node();
+    Node* second = new Node();
+    Node* third = new Node();
+    Node* forth = new Node();
+
+    // ------------------------------
+    // Store data in each node
+    // ------------------------------
+    first->data = 10;
+    second->data = 20;
+    third->data = 30;
+    forth->data =  40;
+
+    // ------------------------------
+    // Connect the nodes
+    // first → second → third → NULL
+    // ------------------------------
+    first->next = second;
+    second->next = third;
+    third->next = forth;
+    forth->next = NULL;
+
+    // ------------------------------
+    // Traversal
+    // temp starts from first node
+    // Move until temp becomes NULL
+    // ------------------------------
+    Node* temp = first;
+
+    cout << "Linked List: ";
+
+    while (temp != NULL) {
+        cout << temp->data << " ";
+        temp = temp->next;   // Move to next node
+    }
+
+    cout << endl;
+
+    return 0;
+}
